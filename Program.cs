f7eb0ed18501737b6e0aec69b0097e2e359dc3b2
AmirabbasDorasay <46056699+AmirabbasDorasay@users.noreply.github.com>
2021-02-21 21:50:04 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BinaryToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable section
+            string binaryInput;
+            int Converter;
+
+            //convert and input section
+            Console.Write("Binary Ra Vared Konid : ");
+            binaryInput = Console.ReadLine();
+            Converter = Convert.ToInt32(binaryInput, 2);
+
+            //output section
+            Console.WriteLine(Converter);
+
+
+            Console.ReadKey();
+        }
+    }
+}
